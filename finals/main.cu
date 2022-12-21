df3b812fb4366867ba4743dd071672c9c66c5754
guudie <gottychaos@gmail.com>
2022-12-21 23:37:11 +0700

    temp branch added

--- finals/main.cu
+++ finals/main.cu
@@ -232,8 +232,153 @@ void seamCarvingCpu(const uchar3* inPixels, uchar3* outPixels, int width, int he
     free(trace);
 }
 
+__constant__ int d_xSobel[9];
+__constant__ int d_ySobel[9];
+
+__global__ void computeEnergyKernel(const uchar3* inPixels, int* energy, int width, int height) {
+    extern __shared__ uchar3 s_inPixels[];
+    int r = blockIdx.y * blockDim.y + threadIdx.y;
+	int c = blockIdx.x * blockDim.x + threadIdx.x;
+	int dimXWithFilter = blockDim.x + 2;
+	int halfFltWidth = 3 >> 1;
+	int t_r = r >= height ? height - 1 : r;
+	int t_c = c >= width ? width - 1 : c;
+	s_inPixels[(threadIdx.y + halfFltWidth) * dimXWithFilter + threadIdx.x + halfFltWidth] = inPixels[t_r * width + t_c];
+	if(threadIdx.y < halfFltWidth) {
+		// top apron
+		int rt = r - halfFltWidth;
+		rt = rt < 0 ? 0 : rt;
+		s_inPixels[threadIdx.y * dimXWithFilter + threadIdx.x + halfFltWidth] = inPixels[rt * width + t_c];
+
+		// bottom apron
+		int rb = (blockIdx.y + 1) * blockDim.y + threadIdx.y;
+		rb = rb >= height ? height - 1 : rb;
+		s_inPixels[(threadIdx.y + blockDim.y + halfFltWidth) * dimXWithFilter + threadIdx.x + halfFltWidth] = inPixels[rb * width + t_c];
+
+		// left & right aprons
+		int cl = blockIdx.x * blockDim.x - halfFltWidth + threadIdx.y;
+		int cr = (blockIdx.x + 1) * blockDim.x + threadIdx.y;
+		cr = cr >= width ? width - 1 : cr;
+		cl = cl < 0 ? 0 : cl;
+        for(int idx = threadIdx.x; idx < blockDim.y + 2; idx += blockDim.x) {
+            int tmpR = blockIdx.y * blockDim.y + idx - halfFltWidth;
+            tmpR = tmpR < 0 ? 0 : tmpR >= height ? height - 1 : tmpR;
+		    s_inPixels[idx * dimXWithFilter + threadIdx.y] = inPixels[tmpR * width + cl];
+		    s_inPixels[idx * dimXWithFilter + blockDim.x + halfFltWidth + threadIdx.y] = inPixels[tmpR * width + cr];
+        }
+	}
+	__syncthreads();
+
+    if(r < height && c < width) {
+		int x = 0, y = 0;
+		for(int f_r = 0; f_r < 3; f_r++) {
+			for(int f_c = 0; f_c < 3; f_c++) {
+				int ri = threadIdx.y + f_r;
+				int ci = threadIdx.x + f_c;
+				uchar3 val = s_inPixels[ri * dimXWithFilter + ci];
+				x += (val.x + val.y + val.z) / 3 * d_xSobel[f_r * 3 + f_c];
+				y += (val.x + val.y + val.z) / 3 * d_ySobel[f_r * 3 + f_c];
+			}
+		}
+		energy[r * width + c] = abs(x) + abs(y);
+	}
+}
+
+// called with 1 block
+__global__ void computeSeamsKernel(const int* energy, int* dp, int width, int height) {
+    extern __shared__ int s_rows[]; // stores 2 consecutive rows for faster memory access
+    for(int c = threadIdx.x; c < width; c += blockDim.x) {
+        dp[c] = energy[c];
+        s_rows[width + c] = energy[c];
+    }
+    __syncthreads();
+    for(int r = 1; r < height; r++) {
+        for(int c = threadIdx.x; c < width; c += blockDim.x) {
+            int i = r * width + c;
+            int res = s_rows[(r & 1) * width + c];
+            if(c - 1 >= 0)
+                res = min(res, s_rows[(r & 1) * width + c - 1]);
+            if(c + 1 < width)
+                res = min(res, s_rows[(r & 1) * width + c + 1]);
+            res += energy[i];
+            dp[i] = res;
+            s_rows[(1 - (r & 1)) * width + c] = res;
+        }
+        __syncthreads();
+    }
+}
+
+__global__ void minReductionKernel(const int* dp_lastRow, int width, int* blockMin) {
+    extern __shared__ int2 s_data[];
+    int c = blockIdx.x * blockDim.x * 2 + threadIdx.x;
+    if(c < width) {
+        s_data[threadIdx.x].x = dp_lastRow[c];
+        s_data[threadIdx.x].y = c;
+    }
+    if(c + blockDim.x < width) {
+        s_data[threadIdx.x + blockDim.x].x = dp_lastRow[c + blockDim.x];
+        s_data[threadIdx.x + blockDim.x].y = c + blockDim.x;
+    }
+    __syncthreads();
+
+    for(int stride = blockDim.x; stride > 0; stride /= 2) {
+        if(threadIdx.x < stride)
+            if(c + stride < width && s_data[threadIdx.x].x > s_data[threadIdx.x + blockDim.x].x)
+                s_data[threadIdx.x] = s_data[threadIdx.x + blockDim.x];
+        __syncthreads();
+    }
+
+    if(threadIdx.x == 0)
+        blockMin[blockIdx.x] = s_data[0].y;
+}
+
+// inPixels1 contains pixel info for the current iteration, inPixels2 will be calculated to hold info for the next
+__global__ void removeSeamKernel(uchar3* inPixels1, uchar3* inPixels2, int* trace, int width, int height) {
+    int r = blockIdx.y * blockDim.y + threadIdx.y;
+    int c = blockIdx.x * blockDim.x + threadIdx.x;
+    if(r < height && c < width - 1)
+        inPixels2[r * (width - 1) + c] = inPixels1[r * width + c + (c >= trace[r])];
+}
+
+void swapPtr(uchar3*& a, uchar3*& b) {
+    uchar3* tmp = a;
+    a = b;
+    b = tmp;
+}
+
+void seamCarvingGpu(const uchar3* inPixels, uchar3* outPixels, int width, int height, int targetWidth,
+        int* xSobel, int* ySobel, dim3 blockSize=dim3(1))
+{
+    // temp values, used for debugging
+    dim3 blockSizeEnergy(1024, 1024);
+    dim3 blockSizeSeams(1024);
+    dim3 blockSizeReduction(1024);
+    dim3 blockSizeCarve(1024, 1024);
+    ///////////////////////////////////////////
+
+    uchar3 *d_inPixels1, *d_inPixels2;
+    int *d_energy, *d_dp, *d_trace, *d_blockMin;
+    int *blockMin;
+
+    size_t inSize = sizeof(uchar3) * width * height;
+    size_t arrSize = sizeof(int) * width * height;
+
+    dim3 gridSizeEnergy((width - 1) / blockSizeEnergy.x + 1, (height - 1) / blockSizeEnergy.y + 1);
+    dim3 gridSizeSeams(1);
+    dim3 gridSizeReduction((width - 1) / blockSizeReduction.x / 2 + 1);
+    dim3 gridSizeCarve((width - 1) / blockSizeCarve.x + 1, (height - 1) / blockSizeCarve.y + 1);
+
+    CHECK(cudaMalloc(&d_inPixels1, inSize));
+    CHECK(cudaMalloc(&d_inPixels2, inSize));
+
+    cudaMalloc(&d_energy, arrSize);
+    cudaMalloc(&d_dp, arrSize);
+    cudaMalloc(&d_trace, arrSize);
+    cudaMalloc(&d_blockMin, )
+}
+
 void seamCarving(const uchar3* inPixels, uchar3* outPixels, int width, int height, int targetWidth,
-        int* xSobel, int* ySobel, bool useDevice=false) 
+        int* xSobel, int* ySobel, dim3 blockSize=dim3(1), bool useDevice=false) 
 {
     GpuTimer timer;
 	timer.Start();
